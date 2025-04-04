c61438021f014d29e040ef8bd4ff9804818c851e
Kichan-Seoul <kimkc30@icloud.com>
2025-04-04 17:41:37 +0900

    esdfgedsfga

---
+++ 01.my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { React } from 'react';
+// 주석임
+/* 주석임 */
+function App() {
+  return (
+    <>  {/*주석임*/}
+      <h1>React Test Page</h1>
+      <p>오늘부터 리엑트 시작 한다는데 이거 배워서 어따 써먹냐</p>
+    </>
+  );
+}
+
+export default App;
